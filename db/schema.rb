9c8226797995a8d70063c8566340c12e5a2eb6ab
Travis Combs <travisphere@yahoo.com>
2017-03-15 22:56:12 +0000

    added coor field to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314190114) do
+ActiveRecord::Schema.define(version: 20170315220813) do
 
   create_table "object_names", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170314190114) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
   end
 
 end
