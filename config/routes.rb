5d77add5dc176a7170eba1ad74f84ebde9cb7672
Travis Combs <travisphere@yahoo.com>
2017-03-22 17:48:40 +0000

    4.3 take 1

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -64,4 +64,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resourses :orders, only: [:index, :show, :create, :destroy]
 end
